e5f13b59d1b34024af484aa0c89f3bb410096be9
Hadil370 <150466554+Hadil370@users.noreply.github.com>
2023-11-13 17:08:02 +0100

    Update patient_module.js

--- server/modules/patient_module.js
+++ server/modules/patient_module.js
@@ -2,13 +2,38 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const ggggSchema = new Schema({
-
-},
-    {
-        timestamps: true
-    })
-
-const gggg = mongoose.model('gggg', ggggSchema)
-
-module.exports = gggg
+const patientSchema = new mongoose.Schema({
+    ID_Utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' },
+    niveauAddiction: {
+      type: String,
+      required: true,
+    },
+    moyenneHeuresJeuParSemaine: {
+      type: Number,
+      required: true,
+    },
+    moyenneMoisJeu: {
+      type: Number,
+      required: true,
+    },
+    scoreInsomnie: {
+      type: Number,
+      required: true,
+    },
+      
+    scoreSomnolenceExcessive: {
+      type: Number,
+      required: true,
+    },
+    scoreAnxiete: {
+      type: Number,
+      required: true,
+    },
+    scoreDepression: {
+      type: Number,
+      required: true,
+    }
+   
+  });
+   const Patient = mongoose.model('Patient', patientSchema);
+module.exports =Patient;
