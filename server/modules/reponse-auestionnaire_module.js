43e0730f62aabf750294b55c2347124cffc89dcf
Hadil370 <150466554+Hadil370@users.noreply.github.com>
2023-11-13 17:20:44 +0100

    Update reponse-auestionnaire_module.js

--- server/modules/reponse-auestionnaire_module.js
+++ server/modules/reponse-auestionnaire_module.js
@@ -2,13 +2,31 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const ggggSchema = new Schema({
 
-},
-    {
-        timestamps: true
-    })
-
-const gggg = mongoose.model('gggg', ggggSchema)
-
-module.exports = gggg
+ const reponseAuQuestionnaireSchema = new mongoose.Schema({
+    Réponse_à_la_question: {
+      type: String,
+      required: true,
+    },
+    Score_attribué_à_la_réponse: {
+      type: Number,
+      required: true,
+    },
+    Commentaires_sur_la_réponse: {
+      type: String,
+      required: true,
+    },
+  
+      ID_Questionnaire: {
+        type: Schema.Types.ObjectId,
+        ref: 'Questionnaire', 
+        required: true
+      },
+      ID_Question: {
+        type: Schema.Types.ObjectId,
+        ref: 'Question', 
+        required: true
+      },
+    }); 
+const ReponseAuQuestionnaire = mongoose.model('ReponseAuQuestionnaire', reponseAuQuestionnaireSchema);
+module.exports = ReponseAuQuestionnaire;
