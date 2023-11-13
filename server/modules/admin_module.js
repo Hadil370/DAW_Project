f37f95396dea7c94ed6cfe12511125789b460be1
Hadil370 <150466554+Hadil370@users.noreply.github.com>
2023-11-13 17:03:52 +0100

    Update admin_module.js

--- server/modules/admin_module.js
+++ server/modules/admin_module.js
@@ -2,13 +2,24 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const ggggSchema = new Schema({
-
-},
-    {
-        timestamps: true
-    })
-
-const gggg = mongoose.model('gggg', ggggSchema)
-
-module.exports = gggg
+ const adminSchema = new mongoose.Schema({
+    role: {
+      type: String,
+      required: true,
+    },
+    autorisations:{
+           type: [String],
+           default:[],
+    } ,
+   
+    ID_Utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' },
+  });
+  
+  
+  const questionnaireSchema = new mongoose.Schema({
+    dateQuestionnaire: Date,
+  
+    patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
+  });
+  const Admin = mongoose.model('Admin', adminSchema);
+  module.exports =Admin;
