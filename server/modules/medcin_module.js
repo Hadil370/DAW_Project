aa84687bc8313da8f06b0bc5c85ababf83e8d249
Hadil370 <150466554+Hadil370@users.noreply.github.com>
2023-11-13 17:05:21 +0100

    Update medcin_module.js

--- server/modules/medcin_module.js
+++ server/modules/medcin_module.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const ggggSchema = new Schema({
 
-},
-    {
-        timestamps: true
-    })
-
-const gggg = mongoose.model('gggg', ggggSchema)
-
-module.exports = gggg
+const medecinSchema = new mongoose.Schema({
+    specialite: {
+      type: String,
+      required: true,
+    },
+    sessionsTherapiePlanifiees: {
+      type: Number,
+      required: true,
+    },
+    
+    ID_Utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' },
+  });
+   const Medecin = mongoose.model('Medecin', medecinSchema);
+module.exports =Medecin;
