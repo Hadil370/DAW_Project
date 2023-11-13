0588797c32e164839c126c1eb779e746d0ec35e0
Hadil370 <150466554+Hadil370@users.noreply.github.com>
2023-11-13 17:16:47 +0100

    Update questionnaire_module.js

--- server/modules/questionnaire_module.js
+++ server/modules/questionnaire_module.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
-
-const ggggSchema = new Schema({
-
-},
-    {
-        timestamps: true
-    })
-
-const gggg = mongoose.model('gggg', ggggSchema)
-
-module.exports = gggg
+const questionnaireSchema = new mongoose.Schema({
+    ID_Questionnaire: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: mongoose.Types.ObjectId,
+    },
+    ID_Patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
+    dateDuQuestionnaire: {
+      type: Date,
+      required: true,
+    },
+   
+  });
+  
+  const Questionnaire = mongoose.model('Questionnaire', questionnaireSchema);
+  
+  module.exports = Questionnaire;
